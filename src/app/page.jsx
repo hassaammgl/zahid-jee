190f3bd956722736a2051d9d0e4019e0c1a1343f
hassaammgl <h455a4mmehtab@gmail.com>
2025-08-11 20:11:44 +0500

    adding to nav

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <>
-    </>
+    <main className="relative w-screen h-screen overflow-hidden">
+      <Image
+        src="/hero.jpg"
+        alt="hero"
+        fill
+        className="object-cover"
+        quality={100}
+        priority
+      />
+    </main>
   );
 }
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -5,7 +5,7 @@ import { fjallaOne } from "@/app/layout";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-col">
+    <nav className="nav flex flex-col fixed z-10">
       <div className="border-[#ffff00] border-2 w-screen h-1" />
       <div className="flex justify-center items-center gap-4 mt-2">
         <Image
@@ -15,16 +15,53 @@ const Navbar = () => {
           width={50}
           height={50}
         />
-        <h1 className={`text-white ${fjallaOne.variable}`}>ZAHIDJEE TEXTILE MILLS</h1>
+        <h1 className={`font-extrabold title text-[#ffff00] ${fjallaOne.variable}`}>
+          ZAHIDJEE TEXTILE MILLS
+        </h1>
       </div>
-      <div className="text-white flex justify-center items-center mt-4 gap-8">
-        <Link className="hover:text-[#ffff00] transition-colors ease-in-out duration-700" href={"/"}>Home</Link>
-        <Link className="hover:text-[#ffff00] transition-colors ease-in-out duration-700" href={"/divisions"}>Divisions</Link>
-        <Link className="hover:text-[#ffff00] transition-colors ease-in-out duration-700" href={"/products"}>Products</Link>
-        <Link className="hover:text-[#ffff00] transition-colors ease-in-out duration-700" href={"/governance"}>Governance</Link>
-        <Link className="hover:text-[#ffff00] transition-colors ease-in-out duration-700" href={"/investor"}>Investor</Link>
-        <Link className="hover:text-[#ffff00] transition-colors ease-in-out duration-700" href={"/media"}>Media</Link>
-        <Link className="hover:text-[#ffff00] transition-colors ease-in-out duration-700" href={"/contact"}>Contact</Link>
+      <div className="text-white font-light flex justify-center items-center mt-4 gap-8">
+        <Link
+          className=" hover:text-[#ffff00] transition-colors ease-in-out duration-700"
+          href={"/"}
+        >
+          Home
+        </Link>
+        <Link
+          className="hover:text-[#ffff00] transition-colors ease-in-out duration-700"
+          href={"/divisions"}
+        >
+          Divisions
+        </Link>
+        <Link
+          className="hover:text-[#ffff00] transition-colors ease-in-out duration-700"
+          href={"/products"}
+        >
+          Products
+        </Link>
+        <Link
+          className="hover:text-[#ffff00] transition-colors ease-in-out duration-700"
+          href={"/governance"}
+        >
+          Governance
+        </Link>
+        <Link
+          className="hover:text-[#ffff00] transition-colors ease-in-out duration-700"
+          href={"/investor"}
+        >
+          Investor
+        </Link>
+        <Link
+          className="hover:text-[#ffff00] transition-colors ease-in-out duration-700"
+          href={"/media"}
+        >
+          Media
+        </Link>
+        <Link
+          className="hover:text-[#ffff00] transition-colors ease-in-out duration-700"
+          href={"/contact"}
+        >
+          Contact
+        </Link>
       </div>
     </nav>
   );
