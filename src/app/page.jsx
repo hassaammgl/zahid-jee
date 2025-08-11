163d9f9e61371fb114545e7036f302632d7d56c4
hassaammgl <h455a4mmehtab@gmail.com>
2025-08-11 18:55:14 +0500

    setting up routes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,6 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-white text-5xl w-screen h-screen">page</div>
-      <div className="bg-white text-5xl w-screen h-screen">page</div>
     </>
   );
 }
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav>
-      <div className="border-[#ffff00] border-2" />
-      <div></div>
+      <div className="border-[#ffff00] border-2 w-screen" />
+      <Image
+        src={"/logo.png"}
+        alt="Logo"
+        className="inverted"
+        width={2}
+        height={4}
+      />
+      <div>
+        <Link href={""}>Home</Link>
+        <Link href={""}>Divisions</Link>
+        <Link href={""}>Products</Link>
+        <Link href={""}>Governance</Link>
+        <Link href={""}>Investor</Link>
+        <Link href={""}>Media</Link>
+        <Link href={""}>Contact</Link>
+      </div>
     </nav>
   );
 };
